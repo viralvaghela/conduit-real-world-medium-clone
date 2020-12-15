0293aed57680985829b36fa78fc40fd0dd868612
viralvaghela <viralvaghela7109@gmail.com>
2020-12-15 21:24:01 +0530

    home screen create

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,38 @@
+import 'package:conduit/services/User.dart';
+import 'package:conduit/utils/global_data.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-   return Scaffold(
+  _HomeState createState() => _HomeState();
+}
 
-   );
+class _HomeState extends State<Home> {
+  bool isLoading = true;
+
+  @override
+  initState() {
+    super.initState();
+   // User.getCurrentUser();
+    setState(() {
+      isLoading = false;
+    });
   }
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: isLoading == true
+            ? Text("")
+            : Text("${GlobalData.loggedinUser.user.username}"),
+      ),
+      body: isLoading == true
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Text("${GlobalData.loggedinUser.user.email}"),
+    );
+  }
 }
