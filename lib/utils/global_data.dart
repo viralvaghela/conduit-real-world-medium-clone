0cd9a16a930cd093dc8f5eccadb897172d5ab721
viralvaghela <viralvaghela7109@gmail.com>
2020-12-15 21:26:09 +0530

    user object in GlobalData

--- lib/utils/global_data.dart
+++ lib/utils/global_data.dart
@@ -1,5 +1,9 @@
 import 'package:conduit/models/loggedin_user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class GlobalData {
+class GlobalData  {
+
  static LoggedinUser loggedinUser;
+
+ //SharedPreferences prefs = await SharedPreferences.getInstance();
 }
